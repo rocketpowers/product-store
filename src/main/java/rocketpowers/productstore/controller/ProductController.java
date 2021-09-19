d9c239e79b9fcc80e866c0f8947d1399f791c5f1
rocketpowers <rocketpowerswillian@gmail.com>
2021-09-19 09:22:39 -0300

    del insert

--- src/main/java/rocketpowers/productstore/controller/ProductController.java
+++ src/main/java/rocketpowers/productstore/controller/ProductController.java
@@ -3,6 +3,7 @@ package rocketpowers.productstore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import rocketpowers.productstore.model.AnswerModel;
 import rocketpowers.productstore.model.ProductModel;
 import rocketpowers.productstore.repository.ProductRepository;
 
@@ -16,28 +17,51 @@ public class ProductController {
     private ProductRepository actions;
 
     //list
-    @RequestMapping(value="/products",method = RequestMethod.GET)
-    public @ResponseBody List<ProductModel> listar() {
+    @RequestMapping(value = "/products", method = RequestMethod.GET)
+    public @ResponseBody
+    List<ProductModel> listar() {
         return actions.findAll();
 
     }
 
-    @RequestMapping(value="/products", method=RequestMethod.POST)
-    public @ResponseBody ProductModel register(@RequestBody ProductModel product) {
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public @ResponseBody
+    ProductModel register(@RequestBody ProductModel product) {
         return actions.save(product);
 
     }
 
     // Filtrar produtos
-    @RequestMapping(value="/products/{code}", method=RequestMethod.GET)
-    public @ResponseBody ProductModel filter(@PathVariable Integer code) {
+    @RequestMapping(value = "/products/{code}", method = RequestMethod.GET)
+    public @ResponseBody
+    ProductModel filter(@PathVariable Integer code) {
         return actions.findByCode(code);
     }
+
     // Alterproducts
-    @RequestMapping(value="/products", method=RequestMethod.PUT)
-    public @ResponseBody ProductModel alter(@RequestBody ProductModel product) {
+    @RequestMapping(value = "/products", method = RequestMethod.PUT)
+    public @ResponseBody
+    ProductModel alter(@RequestBody ProductModel product) {
         return actions.save(product);
     }
 
+    // Remover produtos
+    @RequestMapping(value = "/products/{code}", method = RequestMethod.DELETE)
+    public @ResponseBody AnswerModel remove(@PathVariable Integer code) {
 
-}
+        AnswerModel answer = new AnswerModel();
+
+        try {
+
+            ProductModel product = filter(code);
+            this.actions.delete(product);
+            answer.setMessage("successfully delete product ");
+        }catch(Exception error) {
+            answer.setMessage("failed to  remove " +error.getMessage());
+
+        }
+            return answer;
+
+        }
+
+    }
---
+++ src/main/java/rocketpowers/productstore/model/AnswerModel.java
@@ -0,0 +1,11 @@
+package rocketpowers.productstore.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class AnswerModel {
+
+    @Getter
+    @Setter
+    private String message;
+}
